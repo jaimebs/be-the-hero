4687743f1a6b1864dba78d5f7a309637509cf223
Jaime Barbosa <jaimebs@gmail.com>
2020-03-30 14:40:14 -0300

    refactor: Ajustes e melhorias.

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -6,7 +6,6 @@ const routes = require('./routes');
 
 app.use(cors());
 app.use(express.json());
-
 app.use(routes);
 
 app.listen(3333, () => {
--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -1,4 +1,8 @@
 const routes = require('express').Router();
+const sessionController = require('./controllers/sessionController');
+const ongController = require('./controllers/ongController');
+const incidentController = require('./controllers/incidentController');
+const profileController = require('./controllers/profileController');
 
 /**
  * Tipos de parâmetros:
@@ -7,9 +11,15 @@ const routes = require('express').Router();
  * Route Params: Parâmetros utilizados para identificar recursos (Rota/Recurso/:id);
  * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos.
  */
+routes.post('/sessions', sessionController.create);
 
-routes.post('/', (req, res) => {
-  res.json({ menssagem: 'Bateu no post! Routes' });
-});
+routes.get('/ongs', ongController.index);
+routes.post('/ongs', ongController.create);
+
+routes.get('/incidents', incidentController.index);
+routes.post('/incidents', incidentController.create);
+routes.delete('/incidents/:id', incidentController.delete);
+
+routes.get('/profile', profileController.index);
 
 module.exports = routes;
